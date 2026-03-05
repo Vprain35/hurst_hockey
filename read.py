fd8668cc126508e5eaccaa167eb4beac50f09d3e
Vprain35 <vprain35@lakers.mercyhurst.edu>
2026-03-05 00:22:02 +0000

    3/4 - query practices w sqlmodel

---
+++ read.py
@@ -0,0 +1,17 @@
+from sqlmodel import Session, select
+from models import Bio, Stats, engine
+from sqlalchemy import func
+import pandas as pd
+
+with Session(engine) as session:
+    statement = (
+        select(Bio)
+    )
+    records = session.exec(statement).all()
+
+records_list = []
+for record in records:
+    records_list.append(record.model_dump())
+
+records_df = pd.DataFrame(records_list)
+print(records_df)
